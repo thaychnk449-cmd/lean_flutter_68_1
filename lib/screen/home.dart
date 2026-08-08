1a011130c401636ccf8534176dd4e7672c47b3cc
Thanchanok <thaychnk449>
2026-08-08 14:04:38 +0700

    Step 6: Image Widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,84 +6,17 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Hello World'),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          color: Colors.red,
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            'Hello World',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-            fontWeight: FontWeight.bold,
-            letterSpacing: 3,
-
-          ),
+        Image.asset('assets/images/flutter.png', width: 200, height: 200),
+        SizedBox(height: 20),
+        Image.network(
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Khaoyai41.jpg/1920px-Khaoyai41.jpg?utm_source=th.wikipedia.org&utm_campaign=index&utm_content=thumbnail',
+          width: 200,
+          height: 200,
         ),
-        Container(
-          color: Colors.green,
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            'Hello World',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-            fontWeight: FontWeight.bold,
-            letterSpacing: 3,
-
-          ),
-        )
-        Container(
-          color: Colors.blue,
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            'Hello World',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-            fontWeight: FontWeight.bold,
-            letterSpacing: 3,
-
-          ),
-        )
       ],
     );
-      
   }
 }
