35867290311fcde3adfcada79878ec651920ba97
Thanchanok <thaychnk449>
2026-08-15 10:43:40 +0700

    Step 9: ListView Widget

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -8,48 +8,50 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  int _counter = 0;
-
-  void incrementCounter() {
-    setState(() {
-      quantity++;
-    });
-  }
-
-  void decrementCounter() {
-    setState(() {
-      if (quantity > 0) {
-        quantity--;
-      }
-    });
-  }
+List data = ["สมชาย", "สหญิง", "สมปอง", "สมศรี", "สมจิต"];  
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Quantity: $quantity',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return ListView.builder();
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(10),
           ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: decrementCounter,
-                child: const Text("Decrement"),
-              ),
-              SizedBox(width: 20),
-              ElevatedButton(
-                onPressed: incrementCounter,
-                child: const Text("Increment"),
-              ),
-            ],
+        margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+        padding: EdgeInsets.all(30),
+        child: Text(
+            data[index],
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-        ],
-      ),
-    )
+        )
+  }
 }
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Text(
+//             'Quantity: $quantity',
+//             style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//           ),
+//           const SizedBox(height: 20),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: decrementCounter,
+//                 child: const Text("Decrement"),
+//               ),
+//               SizedBox(width: 20),
+//               ElevatedButton(
+//                 onPressed: incrementCounter,
+//                 child: const Text("Increment"),
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     )
+// }
